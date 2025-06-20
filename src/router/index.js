4a4395b432233ba2e8a7203b3cafa47dec36cc89
maycorolbuche <mayco_rolbuche@hotmail.com>
2025-06-19 21:33:13 -0300

    Ajuste rota

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '@/views/Home.vue';
-import About from '@/views/About.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
 const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: Home,
-    },
-    {
-        path: '/about',
-        name: 'About',
-        component: About,
-    },
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
 ];
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(import.meta.env.BASE_URL ?? "/"),
+  routes,
 });
 export default router;
