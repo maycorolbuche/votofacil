7f1260aefaa07698c4eb494dc2f762556e8dc3a8
maycorolbuche <mayco_rolbuche@hotmail.com>
2025-06-20 13:48:06 -0300

    Ajuste

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
 <template>
   <router-view></router-view>
 </template>
+
+<style>
+html,
+body {
+  background: #531ea4;
+  height: 100vh;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,12 +6,19 @@
 
 <style scoped>
 .logo {
-  height: 6em;
+  max-width: 200px;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
 }
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+  filter: drop-shadow(0 0 2em #fff);
+}
+@media (max-width: 350px) {
+  .logo {
+    width: 100%;
+    max-width: 100%;
+    padding: 1em;
+  }
 }
 </style>
