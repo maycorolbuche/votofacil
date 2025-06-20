64160efa539641e991874222a48cf1e62541bc0d
maycorolbuche <mayco_rolbuche@hotmail.com>
2025-06-19 21:29:18 -0300

    Ajuste deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: process.env.VITE_BASE_URL ?? "/",
   plugins: [vue()],
   server: {
     port: 4001,
