0190c0565208c77d159ed9fc1fc9fb9a16901cbb
maycorolbuche <mayco_rolbuche@hotmail.com>
2025-06-20 13:03:24 -0300

    Ajuste dir

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,9 @@ const routes = [
   },
 ];
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL ?? "/"),
+  history: createWebHistory(
+    import.meta.env.BASE_URL ?? import.meta.env.VITE_BASE_URL ?? "/"
+  ),
   routes,
 });
 export default router;
