0d0c9961e8ba437e3f6da502a4c4fa2c991b1341
maycorolbuche <mayco_rolbuche@hotmail.com>
2025-06-23 19:37:33 -0300

    Ajuste Babel

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
-  plugins: [
-    // Remove consoles em produção
-    process.env.NODE_ENV === "production" ? "transform-remove-console" : "",
-  ],
-};
