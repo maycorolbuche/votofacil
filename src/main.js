54a3fa586e226cdc5334c872b7f00326a88cff5b
maycorolbuche <mayco_rolbuche@hotmail.com>
2025-07-08 12:27:50 -0300

    Ajuste bootstrap e scss

--- src/main.js
+++ src/main.js
@@ -8,20 +8,7 @@ import "bootstrap-vue-next/dist/bootstrap-vue-next.css";
 
 const app = createApp(App);
 
-// Registrar todos os componentes do Bootstrap
-import * as components from "bootstrap-vue-next";
-Object.entries(components).forEach(([componentName, component]) => {
-  if (componentName.startsWith("B")) {
-    app.component(componentName, component);
-  }
-});
-
-import "./assets/styles/main.css";
-import "./assets/styles/text.css";
-import "./assets/styles/input.css";
-import "./assets/styles/button.css";
-import "./assets/styles/logo.css";
-import "./assets/styles/link.css";
+import "./assets/styles/main.scss";
 
 app.use(router);
 app.use(createBootstrap());
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,19 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve,dirname  } from 'path';
 import { fileURLToPath } from 'url';
+import Components from 'unplugin-vue-components/vite'
+import {BootstrapVueNextResolver} from 'bootstrap-vue-next'
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   base: process.env.VITE_BASE_URL ?? "/",
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [BootstrapVueNextResolver()],
+    }),
+  ],
   server: {
     port: 4001,
   },
